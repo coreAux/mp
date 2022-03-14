bafc60f0f8713c6999d5dfafb9734897e630d667
Mickey <57460035+coreAux@users.noreply.github.com>
2022-03-14 12:30:32 +0000

    changes to gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,32 +15,29 @@ module.exports = {
     },*/
 
     {
-     resolve: "gatsby-plugin-google-gtag",
-     options: {
-       trackingIds: [
-         "GTM-PXXJN64",
-         // G-69YD8ETXJE
-       ],
-       // This object gets passed directly to the gtag config command
-       // This config will be shared across all trackingIds
-       gtagConfig: {
-         // optimize_id: "GTM-PXXJN64",
-         anonymize_ip: true,
-         cookie_expires: 0,
-       },
-       // This object is used for configuration specific to this plugin
-       pluginConfig: {
-         // Puts tracking script in the head instead of the body
-         head: false,
-         // Setting this parameter is also optional
-         respectDNT: true,
-         // Avoids sending pageview hits from custom paths
-         exclude: ["/preview/**", "/do-not-track/me/too/"],
-         // Defaults to https://www.googletagmanager.com
-         origin: "https://www.googletagmanager.com"/*"YOUR_SELF_HOSTED_ORIGIN"*/,
-       },
-     },
-   },
+      resolve: "gatsby-plugin-google-gtag",
+      options: {
+        trackingIds: [
+          // "GTM-PXXJN64",
+          "G-69YD8ETXJE",
+        ],
+        gtagConfig: {
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ["/preview/**", "/do-not-track/me/too/"],
+          // Defaults to https://www.googletagmanager.com
+          origin: "https://www.googletagmanager.com"/*"YOUR_SELF_HOSTED_ORIGIN"*/,
+        },
+      },
+    },
 
     {
       resolve: "gatsby-plugin-react-helmet"
